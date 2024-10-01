149434253a63e79bfe9f7c5dd7ea704735d0766c
yasamin <119296009+yyaass3@users.noreply.github.com>
2024-10-01 20:15:31 +0330

    Create Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -48,4 +48,42 @@ class student:
       studentList.insert(END, rows, str(''))
 
   def StudentRec(event):
-    
+    global sd
+    searchSd = studentList.curselection()[0]
+    sd = studentList.get(searchSd)
+
+    self.txtSdtID.delete(0, END)
+    self.txtSdtID.insert(END, sd[1])
+    self.txtfNa.delete(0, END)
+    self.txtfNa.insert(END, sd[2])
+    self.txtsNa.delete(0, END)
+    self.txtsNa.insert(END, sd[3])
+    self.txtdob.delete(0, END)
+    self.txtdob.insert(END, sd[4])
+    self.txtage.delete(0, END)
+    self.txtage.insert(END, sd[5])
+    self.txtgender.delete(0, END)
+    self.txtgender.insert(END, sd[6])
+    self.txtadr.delete(0, END)
+    self.txtadr.insert(END, sd[7])
+    self.txtmobile.delete(0, END)
+    self.txtmobile.insert(END, sd[8])
+
+def deleteData():
+  if len(StdID.get()) != 0 :
+    StudentDMS.deleteRec(sd[0])
+    clearData()
+    DisplayData()
+
+def searchDatabase():
+  studentList.delete(0, END)
+  for rows in StudentDMS.searchData(StdID.get(), firstName.get(), surName.get(), dob.get(), age.get(), gender.get(), address.get(), mobile.get()):
+    studentList.insert(END, rows, str(''))
+
+def update():
+  if len(StdID.get()) != 0:
+    StudentDMS.deleteRec(sd[0])
+  if len(StdID.get()) != 0:
+    StudentDMS.addStdRec(StdID.get(), firstName.get(), surName.get(), dob.get(), age.get(), gender.get(), address.get(), mobile.get())
+    studentList.delete(0, END)
+    studentList.insert(END, (StdID.get(), firstName.get(), surName.get(), dob.get(), age.get(), gender.get(), address.get(), mobile.get()))
